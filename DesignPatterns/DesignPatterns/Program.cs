ee3ebdc3cb6555ee5ded5f704272463379097e4c
alistin <alistin@DESKTOP-60KU2CP>
2017-03-04 20:19:44 +0500

    add new project

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Введите название шаблона.");
+            input = Console.ReadLine();
+            switch (input) {
+                case "1":
+                    Singleton.getSingletone("Одиночка работает");
+                    Console.WriteLine(Singleton.text);
+                    break;
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// Паттерн проектирования Одиночка (singleton). 
+    /// Гарантирует, что у класса есть только один экземпляр и предоставляет к нему точку доступа.
+    /// Предоставляет глобальную точку доступа к объекту. 
+    /// Можно создать только один объект. 
+    /// </summary>
+    class Singleton
+    {
+        private static Singleton instance;
+        public static string text;
+        
+
+        private Singleton(string a) //конструктор класса приватный
+        {
+            text = a;
+        }
+        public static Singleton getSingletone(string a) //публичный метод, который создает объект, только если он уже не создан
+        {
+            if (instance == null)
+            {
+                instance = new DesignPatterns.Singleton(a);
+            }
+            return instance;
+        }
+    }
+}
